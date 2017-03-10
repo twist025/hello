a342a1e765067811f9cb75920c7b26e0aa44999d
twist025 <twist025@mail.ru>
2017-03-10 11:52:49 +0300

    Update bar.cpp

--- bar.cpp
+++ bar.cpp
@@ -3,8 +3,8 @@ int bar(a,b){
   return a*b;
 }
 
-TEST_CASE("foo work?","[foo]"){
-  REQUIRE(foo(1,2)==2);
-  REQUIRE(foo(2,4)==8);
-  REQUIRE(foo(3,7)==21);
+TEST_CASE("bar work?","[foo]"){
+  REQUIRE(bar(1,2)==2);
+  REQUIRE(bar(2,4)==8);
+  REQUIRE(bar(3,7)==21);
 }
