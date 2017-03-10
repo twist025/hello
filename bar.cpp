9ce7b23a5fb7a1cdc432f07767063d5c65ecfdf5
twist025 <twist025@mail.ru>
2017-03-10 11:51:42 +0300

    Update bar.cpp

--- bar.cpp
+++ bar.cpp
@@ -4,7 +4,7 @@ int bar(a,b){
 }
 
 TEST_CASE("foo work?","[foo]"){
-  REQUIRED(foo(1,2)==2);
-  REQUIRED(foo(2,4)==8);
-  REQUIRED(foo(3,7)==21);
+  REQUIRE(foo(1,2)==2);
+  REQUIRE(foo(2,4)==8);
+  REQUIRE(foo(3,7)==21);
 }
